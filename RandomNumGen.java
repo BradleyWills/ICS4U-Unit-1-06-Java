3add3149bb613ce41747e01ed6353eeadec7d440
Bradley Wills <bradley.wills@mths.ca>
2020-03-02 02:21:54 +0000

    initial commit

---
+++ RandomNumGen.java
@@ -0,0 +1,46 @@
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+* The RandomNumGen program implements an application that generates a random 
+* number based on the inputed max and evaluates if the user guesses it.
+*
+* @author  Bradley Wills
+* @version 1.0
+* @since   2020-03-01 
+*/
+
+public class RandomNumGen {
+  // Create variables 
+  static int randomizedNum;
+  static int guesses;
+  static int maxValueInput;
+  static boolean correct;
+  
+  public static void RollDie(int maxValue) {
+    Random randomizer = new Random();
+    // Generate random number 1-maxValue
+    randomizedNum = (randomizer.nextInt(maxValue)) + 1;
+    // Prints the random number
+    System.out.println(randomizedNum);
+  }
+  
+  /**
+  * Calculates and checks if the users input is the same as the guess.
+  */
+  
+  public static void main(String[] args) {
+    // Create scanner and randomizer
+    Scanner userInput = new Scanner(System.in);
+    // Ask for input
+    System.out.println("Input the max value.");
+    try {
+      maxValueInput = Integer.parseInt(userInput.nextLine());
+      // Calls the function
+      RollDie(maxValueInput);
+    } catch (Exception e) {
+      // Informs the user that their input is invalid
+      System.out.println("Invalid Input.");
+    }
+  }
+}
